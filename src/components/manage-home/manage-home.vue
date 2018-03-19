b05ebcf1cebcc5df1e71af4980510a7547bce875
luoxuan <15829289428@163.com>
2018-03-19 10:05:47 +0800

    fixed

--- src/components/manage-home/manage-home.vue
+++ src/components/manage-home/manage-home.vue
@@ -1,18 +1,363 @@
 <template>
-  <div class="">manage-home</div>
+  <div class="manage-home">
+    <div class="header clearfix">
+      <img src="@/assets/img/logo.png"/>
+      <div class="title">科研业务综合管理系统</div>
+    </div>
+    <div class="nav">
+      <div class="head-v3">
+      	<div class="navigation-up">
+      		<div class="navigation-inner">
+      			<div class="navigation-v3 clearfix">
+      				<ul>
+
+                <li v-if="status == 1" _t_nav="home">
+      						<h2>
+      							<a href="#">首页</a>
+      						</h2>
+      					</li>
+
+      					<li v-if="status == 2" _t_nav="manageHome">
+      						<h2>
+      							<a href="#">管理首页</a>
+      						</h2>
+      					</li>
+
+                <li v-if="status == 1" _t_nav="detail">
+      						<h2>
+      							<a href="#">项目详情</a>
+      						</h2>
+      					</li>
+
+      					<li v-if="status == 2" _t_nav="product">
+      						<h2>
+      							<a href="#">项目管理</a>
+      						</h2>
+      					</li>
+
+                <li v-if="status == 1" _t_nav="rewardDetail">
+      						<h2>
+      							<a href="#">奖励详情</a>
+      						</h2>
+      					</li>
+
+      					<li v-if="status == 2" _t_nav="rewardReview">
+      						<h2>
+      							<a href="#">奖励审核</a>
+      						</h2>
+      					</li>
+
+      					<li _t_nav="search">
+      						<h2>
+      							<a href="#">条件查询</a>
+      						</h2>
+      					</li>
+
+      					<li v-if="status == 2" _t_nav="user">
+      						<h2>
+      							<a href="#">用户管理</a>
+      						</h2>
+      					</li>
+
+      					<li v-if="status == 2" _t_nav="data">
+      						<h2>
+      							<a href="#">数据配置</a>
+      						</h2>
+      					</li>
+                <li _t_nav="news">
+      						<h2>
+      							<a href="#">文档通知</a>
+      						</h2>
+      					</li>
+                <li _t_nav="goto">
+      						<h2>
+      							<a v-if="status == 1" href="#">进入后台审核</a>
+                    <a v-if="status == 2" href="#">进入前台录入</a>
+      						</h2>
+      					</li>
+      				</ul>
+      			</div>
+      		</div>
+      	</div>
+      	<div class="navigation-down">
+
+          <div v-if="status == 1" id="home" class="nav-down-menu menu-1" style="display: none;" _t_nav="home">
+
+      			<div class="navigation-down-inner">
+
+      				<dl style="margin-left: 130px;">
+      					<dt>项目录入</dt>
+      					<dd>
+      						<a hotrep="hp.header.home.compute1" href="#">新增项目</a>
+      					</dd>
+      				</dl>
+
+      				<dl>
+      					<dt>成果录入</dt>
+      					<dd>
+      						<a hotrep="hp.header.home.storage1" href="#">新增获奖</a>
+      					</dd>
+
+      					<dd>
+      						<a hotrep="hp.header.home.storage2" href="#">新增论文</a>
+      					</dd>
+
+      					<dd>
+      						<a hotrep="hp.header.home.storage4" href="#">新增论文</a>
+      					</dd>
+
+      					<dd>
+      						<a hotrep="hp.header.home.storage3" href="#">新增知识产权</a>
+      					</dd>
+
+                <dd>
+      						<a hotrep="hp.header.home.storage3" href="#">新增标准</a>
+      					</dd>
+
+                <dd>
+      						<a hotrep="hp.header.home.storage3" href="#">新增创作成果</a>
+      					</dd>
+      				</dl>
+
+      				<dl>
+      					<dt>关于个人</dt>
+      					<dd>
+      						<a hotrep="hp.header.home.monitoring1" href="#">个人信息维护</a>
+      					</dd>
+
+      				</dl>
+      			</div>
+      		</div>
+
+          <div v-if="status == 2" id="manageHome" class="nav-down-menu menu-1" style="display: none;" _t_nav="manageHome">
+
+      			<div class="navigation-down-inner">
+
+      				<dl style="margin-left: 135px;">
+      					<dt>项目管理</dt>
+      					<dd>
+      						<a hotrep="hp.header.manageHome.compute1" href="#">审核立项</a>
+      					</dd>
+
+      					<dd>
+      						<a hotrep="hp.header.manageHome.compute2" href="#">过程检查</a>
+      					</dd>
+
+      					<dd>
+      						<a hotrep="hp.header.manageHome.compute3" href="#">审核结题</a>
+      					</dd>
+
+      				</dl>
+
+      				<dl>
+      					<dt>奖励审核</dt>
+      					<dd>
+      						<a hotrep="hp.header.manageHome.storage1" href="#">审核项目</a>
+      					</dd>
+
+      					<dd>
+      						<a hotrep="hp.header.manageHome.storage2" href="#">审核获奖</a>
+      					</dd>
+
+      					<dd>
+      						<a hotrep="hp.header.manageHome.storage4" href="#">审核论文</a>
+      					</dd>
+
+      					<dd>
+      						<a hotrep="hp.header.manageHome.storage3" href="#">审核知识产权</a>
+      					</dd>
+
+                <dd>
+      						<a hotrep="hp.header.manageHome.storage3" href="#">审核标准</a>
+      					</dd>
+
+                <dd>
+      						<a hotrep="hp.header.manageHome.storage3" href="#">审核创作成果</a>
+      					</dd>
+      				</dl>
+
+      				<dl>
+      					<dt>关于个人</dt>
+      					<dd>
+      						<a hotrep="hp.header.manageHome.monitoring1" href="#">个人信息维护</a>
+      					</dd>
+      				</dl>
+
+      			</div>
+
+      		</div>
+
+      		<div id="rewardReview" class="nav-down-menu menu-3 menu-1" style="display: none;" _t_nav="rewardReview">
+
+      			<div class="navigation-down-inner">
+
+      				<dl style="margin-left: 90px;">
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardReview.1" href="#">科研项目</a>
+      					</dd>
+
+      				</dl>
+
+      				<dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardReview.2" href="#">科研获奖</a>
+      					</dd>
+      				</dl>
+
+      				<dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardReview.3" href="#">学术论文</a>
+      					</dd>
+      				</dl>
+
+              <dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardReview.3" href="#">学术专著</a>
+      					</dd>
+      				</dl>
+
+              <dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardReview.3" href="#">知识产权</a>
+      					</dd>
+      				</dl>
+
+              <dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardReview.3" href="#">技术标准</a>
+      					</dd>
+      				</dl>
+
+              <dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardReview.3" href="#">创作成果</a>
+      					</dd>
+      				</dl>
+      			</div>
+      		</div>
+
+          <div id="rewardDetail" class="nav-down-menu menu-3 menu-1" style="display: none;" _t_nav="rewardReview">
+
+      			<div class="navigation-down-inner">
+
+      				<dl style="margin-left: 80px;">
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardDetail.1" href="#">科研项目</a>
+      					</dd>
+      				</dl>
+
+      				<dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardDetail.2" href="#">科研获奖</a>
+      					</dd>
+      				</dl>
+
+      				<dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardDetail.3" href="#">学术论文</a>
+      					</dd>
+      				</dl>
+
+              <dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardDetail.3" href="#">学术专著</a>
+      					</dd>
+      				</dl>
+
+              <dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardDetail.3" href="#">知识产权</a>
+      					</dd>
+      				</dl>
+
+              <dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardDetail.3" href="#">技术标准</a>
+      					</dd>
+      				</dl>
+
+              <dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.rewardDetail.3" href="#">创作成果</a>
+      					</dd>
+      				</dl>
+      			</div>
+      		</div>
+
+      		<div v-if="status == 2" id="search" class="nav-down-menu menu-3 menu-1" style="display: none;" _t_nav="search">
+      			<div class="navigation-down-inner">
+
+      				<dl style="margin-left: 415px;">
+      					<dd>
+      						<a class="link" hotrep="hp.header.search.1" href="#">项目查询</a>
+      					</dd>
+      				</dl>
+
+      				<dl>
+      					<dd>
+      						<a class="link" hotrep="hp.header.search.2" href="#">奖励查询</a>
+      					</dd>
+      				</dl>
+
+      			</div>
+      		</div>
+
+      	</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      radio: '1'
+      status: 2
     }
   },
   methods: {
 
   },
   created() {
+    jQuery(document).ready(function(){
+
+    	let qcloud = {};
+
+    	$('[_t_nav]').hover(function(){
+
+    		let _nav = $(this).attr('_t_nav');
+
+    		clearTimeout( qcloud[ _nav + '_timer' ] );
+
+    		qcloud[ _nav + '_timer' ] = setTimeout(function(){
+
+    		$('[_t_nav]').each(function(){
+
+    		$(this)[ _nav == $(this).attr('_t_nav') ? 'addClass':'removeClass' ]('nav-up-selected');
+
+    		});
+
+    		$('#'+_nav).stop(true,true).slideDown(400);
+
+    	}, 200);
+
+    	},function(){
+
+    		let _nav = $(this).attr('_t_nav');
+
+    		clearTimeout( qcloud[ _nav + '_timer' ] );
+
+    		qcloud[ _nav + '_timer' ] = setTimeout(function(){
+
+    		$('[_t_nav]').removeClass('nav-up-selected');
+
+    		$('#'+_nav).stop(true,true).slideUp(400);
+
+    	}, 200);
+
+    	});
+
+    });
 
   }
 }
